8e400fbb8cb0bae30746cfb1bafa7419f0bfcf84
jlo-tech <arch@archbox.fritz.box>
2023-12-31 16:52:35 +0100

    Refactoring.

--- include/multiboot.h
+++ include/multiboot.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <types.h>
+
 // Multiboot2 information struct
 struct multiboot_information
 {
@@ -23,39 +25,6 @@ struct multiboot_memory_map_entry
     u32 reserved;
 } __attribute__((packed));
 
-struct multiboot_information* multiboot_next_tag(struct multiboot_information *mb_info)
-{
-    u32 mask = ~0b111;
-    
-    u32 p = (u32)mb_info;
-    p = p + mb_info->size;
-
-    if((p & mask) != 0)
-    {
-        p = (p & mask) + 8;
-    }
-
-    return (struct multiboot_information*)p;
-}
-
-struct multiboot_memory_map* multiboot_memmap(struct multiboot_information* mb_info)
-{
-    while(mb_info->type != 6)
-    {
-        mb_info = multiboot_next_tag(mb_info);
-    }
-
-    return (struct multiboot_memory_map*)mb_info;
-}
-
-u64 multiboot_memmap_num_entries(struct multiboot_memory_map* memmap)
-{
-    return (memmap->size - 16) / memmap->entry_size;
-}
-
-struct multiboot_memory_map_entry* multiboot_memmap_entry(struct multiboot_memory_map *memmap, u64 index)
-{
-    u64 num_entries = multiboot_memmap_num_entries(memmap);
-    index = index % num_entries;
-    return memmap + 16 + index * memmap->entry_size;
-}
+struct multiboot_memory_map* multiboot_memmap(struct multiboot_information* mb_info);
+u64 multiboot_memmap_num_entries(struct multiboot_memory_map* memmap);
+struct multiboot_memory_map_entry* multiboot_memmap_entry(struct multiboot_memory_map *memmap, u64 index);
